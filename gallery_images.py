9f6bf67e25f9f38d4153aa0ced6d19608aa865ea
Kiyu <qconnection@gmail.com>
2014-07-14 14:39:31 +0200

    Inserted ValueError exception

--- gallery_images.py
+++ gallery_images.py
@@ -1,8 +1,13 @@
 import sys
 import re
 
+#from threading import Timer
+import time
+
 import webbrowser
 import pyimgur
+
+print "Version 2"
 
 CLIENT_ID = "5551c39c732cf55"
 CLIENT_SECRET = "ee9c26ce3daa35c4950407c5e8e1670431f1214f"
@@ -24,18 +29,28 @@ def authorisation():
 	im.exchange_pin(pin)
 	yolo_police()
 
+def comment():
+	pass
+
 def yolo_police():
 	gallery = im.get_gallery(section=img_section, sort=img_sort, window=window, show_viral=True, limit=limit_requests)
 
 	for image in gallery:
 		img_comments = image.get_comments()
 
-		for comment in img_comments:
-			if yolo.search(comment.text):
+	for comment in img_comments:
+		# comment = comment.encode('UTF-8')
+		if yolo.search(comment.text):
+			try:
 				encoded_text = comment.text.encode('UTF-8')
-				print str(comment.id) + ": " + encoded_text + "\n"
+				print str(comment.id) + ": " + str(comment.text) + "\n"
 				comment.reply(botReply)
+				time.sleep(7)
 				break
+			except ValueError:
+				print "ValueError raised. \n"
+				break
+
 
 if __name__ == '__main__':
 	authorisation()
