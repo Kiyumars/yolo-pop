adadab17fe8ff293e769c191edada915624862d6
Kiyu <qconnection@gmail.com>
2014-07-15 16:12:29 +0200

    Now only one yolo reply per comment, now I need to refactor code

--- gallery_images.py
+++ gallery_images.py
@@ -1,8 +1,6 @@
 import sys
 import re
 import itertools
-
-#from threading import Timer
 import time
 
 import webbrowser
@@ -13,7 +11,7 @@ CLIENT_ID = "5551c39c732cf55"
 CLIENT_SECRET = "ee9c26ce3daa35c4950407c5e8e1670431f1214f"
 
 im = pyimgur.Imgur(CLIENT_ID, CLIENT_SECRET)
-#im = pyimgur.Imgur(CLIENT_ID)
+# im = pyimgur.Imgur(CLIENT_ID)
 
 #phrase we are searching for and reply we will post
 yolo = re.compile(r'yolo', re.IGNORECASE)
@@ -43,64 +41,65 @@ def yolo_police():
 	total_img_comments = []
 	yolo_amount = 0
 	checking_amount = 0
+	
 
 	#search image comments and only reply to one yolo comment per image
 	for image in gallery:
 		try:
 			comments_storage = image.get_comments()
 
+
 			for ind_comments in comments_storage:
-				total_img_comments += 1
+				#just tracking all comments in separate list
+				total_img_comments.append(ind_comments.text)
 
-				if yolo.search(ind_comments):
-					
+				#add comment to list only if it matches yolo, only one entry per image
+				if yolo.search(ind_comments.text):
+					img_comments.append(ind_comments)
+					break
+				else:
+					continue
 
 
- 
-			# for storage_entries in comments_storage:
-			# 	storage_entries.text
-			# 	img_comments.append(storage_entries)
-			# comments_storage = []
 		except ValueError:
 			print "ValueError. It happens. \n"
 
+		
 	#create a flatted list from the nested list in comments_storage
-	img_comments = list(itertools.chain.from_iterable(comments_storage))
+	#img_comments = list(itertools.chain.from_iterable(comments_storage))
 
 	#print statements for monitoring purposes
 	print str(len(gallery)) + " pictures in your gallery."
-	print str(len(comments_storage)) + " items in comments_storage."
+	flat_gallery = list(itertools.chain.from_iterable(total_img_comments))
+	print str(len(flat_gallery)) + " total comment items."
 	print str(len(img_comments)) + " items in img_comments."
-	print "Img comments is a " + str(type(img_comments))
-	print "comments_storage is a " + str(type(comments_storage))
+	# print "Img comments is a " + str(type(img_comments))
+	# print "comments_storage is a " + str(type(comments_storage))
 	#print img_comments
 	#print str(len(img_comments)) + " comments in img_comments."
 	#print img_comments
 
-	#search all comments for the term yolo, print out the matched results
-
 	for comment in img_comments:
 		try:
-		# comment = comment.encode('UTF-8')
-			if yolo.search(comment.text):
-				#you might have to use the decode function on comment.text
-				print str(comment.id) + ": " + str(comment.text) + "\n"
-				comment.reply(botReply)
-				time.sleep(7)
-				yolo_amount += 1
-				break
-
+		
+			#you might have to use the decode function on comment.text
+			print str(comment.id) + ": " + str(comment.text) + "\n"
+			comment.reply(botReply)
+			time.sleep(7)
+			yolo_amount += 1
 		except UnicodeEncodeError:
 			print "There was an unicode error."
 			continue
+		except AttributeError:
+			print "AttributeError. Skipping it."
 
 	if yolo_amount < 1:
 		print "Tonto no find Yolos out in wild. Tonto go sleep, try again in hour."
 	else:
 		print str(yolo_amount) + " found, Lone Ranger. Must wait hour, find new Yolos."
 
-	#start program again in an hour
-	time.sleep(60*60)
+	# start program again in an hour
+	time.sleep(60*1)
 	print "Tonto go look for Yolo tracks again, Lone Ranger."
 	yolo_again()
 
@@ -111,4 +110,4 @@ def yolo_again():
 
 if __name__ == '__main__':
 	authorisation()
-	#yolo_police()
+	# yolo_police()
