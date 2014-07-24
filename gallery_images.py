711c297718975268989c9cbb08ece7a8042463ee
Kiyu <qconnection@gmail.com>
2014-07-24 15:39:47 +0200

    Fixed console printouts

--- gallery_images.py
+++ gallery_images.py
@@ -6,6 +6,7 @@ import time
 import webbrowser
 
 import pyimgur
+from tornado.ioloop import IOLoop
 from PIL import Image, ImageDraw, ImageFont
 
 
@@ -22,8 +23,8 @@ Limit_requests = 20
 Frontpage = 'hot'
 Viral_or_new = 'viral'
 How_long = 'day'
-Reply_delay = 7
-Sleeping_time = 60*60
+REPLY_DELAY = 60
+Sleeping_time = 60*1
 Uploading_image = '/home/kiyu/Projects/Imgur/yolopopo/personalised_yolo'
 
 ############## Helper functions
@@ -34,32 +35,31 @@ def authorisation():
 	webbrowser.open(auth_url)
 	pin = raw_input('Tonto help Lone Ranger find Yolo beasts. Tonto need authorisation pin. Lone Ranger need to look at new screen.\n')
 	img.exchange_pin(pin)
+	print "Tonto go out find Yolo beasts. Do not follow Tonto. Too dangerous for Lone Ranger.\n"
 
 
-def get_image_comments(gallery_images):
+
+def process_gallery(gallery_images):
 	"""search image comments and only reply to one yolo comment per image"""
 	total_yolos = 0
 	for image in gallery_images:
-		yolo_addition = search_comments(image.get_comments())
-		total_yolos += yolo_addition
+		yolo_found = search_comments(image.get_comments(), total_yolos * REPLY_DELAY)
+		if yolo_found:
+			total_yolos += 1
+		
 	get_yolo_amount(total_yolos)
 
-def search_comments(comments):
+def search_comments(comments, delay):
 	"""Search every comment for the regex pattern"""
-	yolo_amount = 0
+	loopy = IOLoop.current()
 	for comment in comments:
 		if Yolo.search(comment.text):
-			yolo_amount += 1
 			make_pic(comment.author.name)
 			reply_link = upload_an_image()
-			bot_replies(comment, reply_link)
-			break
-		else:
-			continue
-	return yolo_amount
+			loopy.call_later(delay, reply_to_comment, comment, reply_link)
+			return True
 
-
-def bot_replies(comment, reply_link):
+def reply_to_comment(comment, reply_link):
 	"""bot replies to top yolo comments"""
 	try:
 		#this is just for my own edification
@@ -68,11 +68,10 @@ def bot_replies(comment, reply_link):
 		comment_id = str(comment.id)
 		text = comment.text
 		print comment_id + ": " + text + " " + author + " " + author_id + "\n"
-		
+		print "Tonto kill Yolo beast. See dead yolo at " + comment.permalink + ". \n"
 		#this actually posts the reply
 		comment.reply(reply_link)
 		make_pic(author)
-		time.sleep(Reply_delay)
 		# yolo_amount += 1
 	except UnicodeEncodeError:
 		print "There was an unicode error."
@@ -80,12 +79,6 @@ def bot_replies(comment, reply_link):
 	except AttributeError:
 		print "AttributeError. Skipping it."
 		# continue	
-
-
-def yolo_again():
-	"""imgur access tokens only last an hour, so we need to refresh it"""
-	img.refresh_access_token()
-	yolo_police()
 
 
 def get_images():
@@ -98,8 +91,8 @@ def yolo_police():
 
 	#get all the gallery objects that fit the sorting criteria
 	gallery = get_images()
-
-	get_image_comments(gallery)
+	process_gallery(gallery)
+	IOLoop.current().call_later(60 * 60, yolo_police)
 
 def make_pic(username):
 	"""Caption a pic or gif with the name of the author of the matched comment"""
@@ -114,8 +107,8 @@ def make_pic(username):
 	draw = ImageDraw.Draw(image)
 	font = ImageFont.truetype("/usr/share/fonts/CODE Bold.otf", 45)
 
-	draw.text((0,0), upper_msg, 'white', font=font)
-	draw.text((0,600), lower_msg, 'white', font=font)
+	draw.text((0,0), upper_msg, 'yellow', font=font)
+	draw.text((0,600), lower_msg, 'yellow', font=font)
 
 	image.save('personalised_yolo', 'PNG')
 	# upload_an_image()
@@ -130,17 +123,26 @@ def get_yolo_amount(yolo_amount):
 		print "Tonto no find Yolos in wild.\n"
 	else:
 		print "Tonto find " + str(yolo_amount) + " Yolos out in wild.\n"
+		print "Tonto now stalk Yolos, will strike when Yolos weak.\n"
 
 
 ########### Impressed? I thought so
 
 if __name__ == '__main__':
-	#see how this works if you enter a false pin
-	authorisation()
-
-	while True:
-		print "Tonto go out find Yolo beasts. Do not follow Tonto. Too dangerous for Lone Ranger."
+	# see how this works if you enter a false pin
+	def start_app():
+		authorisation()
 		yolo_police()
-		print "Tonto go sleep one hour. Then find Yolo again.\n"
-		time.sleep(Sleeping_time)
-		img.refresh_access_token()
+
+	loopy = IOLoop.current()
+	loopy.call_later(0, start_app)
+	loopy.start()
+
+	# authorisation()
+
+	# while True:
+	# 	print "Tonto go out find Yolo beasts. Do not follow Tonto. Too dangerous for Lone Ranger."
+	# 	yolo_police()
+	# 	print "Tonto go sleep one hour. Then find Yolo again.\n"
+	# 	time.sleep(Sleeping_time)
+	# 	img.refresh_access_token()
